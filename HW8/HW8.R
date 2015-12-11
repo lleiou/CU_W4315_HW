6f5e27e8894351df7ad21aa43530ccaec0906cf1
lleiou <lleiou@users.noreply.github.com>
2015-12-10 21:56:50 -0500

    Update HW8.R

--- HW8/HW8.R
+++ HW8/HW8.R
@@ -1,3 +1,4 @@
+
 data<-read.table("patient_satisfaction.txt", header=T)
 y<-data[,1]
 x1<-data[,2]
@@ -7,7 +8,7 @@ x3 <-data[,4]
 
 #1a
 fit<-lm(y~x1+x2+x3)
-library(car)
+library(car) 
 residualPlots(fit)
 
 
@@ -20,7 +21,29 @@ ncvTest(fit,~x1+x2+x3)
 
 
 
+#2a
+rstudent(fit)
+library(car) 
+outlierTest(fit,cutoff=1)
 
+#2b
+
+influence(fit)$hat
+
+
+#2c
+xnew<-c(30,58,2)
+t(xnew)%*%solve((t(X)%*%X))%*%xnew
+max(influence(fit)$hat)
+
+
+#2d
+
+
+cooks.distance(fit)
+cook<-cook.distance(fit)
+cook[order(cook, decreasing=T)]
+infIndexPlot(fit)
 
 
 
@@ -40,6 +63,25 @@ ncvTest(fit,~x1+x2+x3)
 
 
 
+#3C
+rstudent(fit)
+library(car) 
+outlierTest(fit,cutoff=1)
+
+
+#3D
+influence(fit)$hat
+hat<-influence(fit)$hat
+hat[order(hat,decreasing=T)]
+
+#3E
+
+cooks.distance(fit)
+cook<-cooks.distance(fit)
+cook[order(cook, decreasing=T)]
+infIndexPlot(fit)
+
+
 
 #4a
 data<-read.table("SENIC.txt", header=T)
@@ -53,3 +95,22 @@ residualPlots(fit)
 
 #4b
 ncvTest(fit,~x1+x2+x3)
+
+
+#4C
+rstudent(fit)
+library(car) 
+outlierTest(fit,cutoff=1)
+
+
+#4D
+influence(fit)$hat
+hat<-influence(fit)$hat
+hat[order(hat,decreasing=T)]
+
+#4E
+
+cooks.distance(fit)
+cook<-cooks.distance(fit)
+cook[order(cook, decreasing=T)]
+infIndexPlot(fit)
